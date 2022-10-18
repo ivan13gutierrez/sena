ced81ad1c88a1d44634a96ca46267b4683e8f63d
ivan13gutierrez <ivancuello92@gmail.com>
2022-10-18 10:45:27 -0500

    cambiando los valores de conexion

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
-/* $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'6toWNLU7CnkKYvs2yG1v','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/cscs/');
+$dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'dev_oretnom','password'=>'6toWNLU7CnkKYvs2yG1v','last_login'=>'','date_updated'=>'','date_added'=>'');
+if(!defined('base_url')) define('base_url','bhhfrot5fnuyytk5xhq6-mysql.services.clever-cloud.com');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 // if(!defined('dev_data')) define('dev_data',$dev_data);
 if(!defined('DB_SERVER')) define('DB_SERVER',"bhhfrot5fnuyytk5xhq6-mysql.services.clever-cloud.com");
 if(!defined('DB_USERNAME')) define('DB_USERNAME',"usc52f0tmhrafirw");
 if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"6toWNLU7CnkKYvs2yG1v");
-if(!defined('DB_NAME')) define('DB_NAME',"cscs_db"); */
+if(!defined('DB_NAME')) define('DB_NAME',"cscs_db");
 ?>
